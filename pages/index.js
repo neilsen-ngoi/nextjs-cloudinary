a1ff8cf23b12006a459ad7031bca081598d9d933
Neilsen Wei <neilsen.nngoi@gmail.com>
2023-10-12 17:41:12 +1300

    cloudinary you bitch

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
-  const [imageSrc, setImageSrc] = useState();
-  const [uploadData, setUploadData] = useState();
+  const [imageSrc, setImageSrc] = useState()
+  const [uploadData, setUploadData] = useState()
 
   /**
    * handleOnChange
@@ -12,14 +12,14 @@ export default function Home() {
    */
 
   function handleOnChange(changeEvent) {
-    const reader = new FileReader();
+    const reader = new FileReader()
 
-    reader.onload = function(onLoadEvent) {
-      setImageSrc(onLoadEvent.target.result);
-      setUploadData(undefined);
+    reader.onload = function (onLoadEvent) {
+      setImageSrc(onLoadEvent.target.result)
+      setUploadData(undefined)
     }
 
-    reader.readAsDataURL(changeEvent.target.files[0]);
+    reader.readAsDataURL(changeEvent.target.files[0])
   }
 
   /**
@@ -28,7 +28,25 @@ export default function Home() {
    */
 
   async function handleOnSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
+    const form = event.currentTarget
+    const fileInput = Array.from(form.elements).find(
+      ({ name }) => name === 'file'
+    )
+    const formData = new FormData()
+
+    for (const file of fileInput.files) {
+      formData.append('file', file)
+    }
+    const data = await fetch(
+      'https://api.cloudinary.com/v1_1/demo/image/upload',
+      {
+        method: 'POST',
+        body: formData,
+      }
+    ).then((r) => r.json())
+
+    console.log('data', data)
   }
 
   return (
@@ -40,21 +58,22 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Image Uploader
-        </h1>
+        <h1 className={styles.title}>Image Uploader</h1>
 
-        <p className={styles.description}>
-          Upload your image to Cloudinary!
-        </p>
+        <p className={styles.description}>Upload your image to Cloudinary!</p>
 
-        <form className={styles.form} method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+        <form
+          className={styles.form}
+          method="post"
+          onChange={handleOnChange}
+          onSubmit={handleOnSubmit}
+        >
           <p>
             <input type="file" name="file" />
           </p>
-          
+
           <img src={imageSrc} />
-          
+
           {imageSrc && !uploadData && (
             <p>
               <button>Upload Files</button>
@@ -62,13 +81,18 @@ export default function Home() {
           )}
 
           {uploadData && (
-            <code><pre>{JSON.stringify(uploadData, null, 2)}</pre></code>
+            <code>
+              <pre>{JSON.stringify(uploadData, null, 2)}</pre>
+            </code>
           )}
         </form>
       </main>
 
       <footer className={styles.footer}>
-        <p>Find the tutorial on <a href="https://spacejelly.dev/">spacejelly.dev</a>!</p>
+        <p>
+          Find the tutorial on{' '}
+          <a href="https://spacejelly.dev/">spacejelly.dev</a>!
+        </p>
       </footer>
     </div>
   )
